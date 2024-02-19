a670304c69ed2356f55eb5bcf8cdf4b5687ba9b9
Urszula Majcher <ulunka.m@gmail.com>
2024-02-19 21:38:07 +0100

    added calculateResult finction

--- js/script.js
+++ js/script.js
@@ -1,34 +1,29 @@
-console.log("Hejka =]");
+{
+  const welcome = () => {
+    console.log("Hejka =]");
+  };
 
-const formElement = document.querySelector(".js-form");
-const amountElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
-const button = document.querySelector(".js-button");
-const resultElement = document.querySelector(".js-result");
+  const formElement = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const button = document.querySelector(".js-button");
+  const resultElement = document.querySelector(".js-result");
 
-const rateUSD = 4.01;
-const rateEUR = 4.32;
-const rateGBP = 5.06;
+  const calculateResult = (amount, currency) => {
+    return amount / currency;
+  };
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
-  let result;
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+    const result = calculateResult(amount, currency);
 
-  switch (currency) {
-    case "USD":
-      result = amount / rateUSD;
-      break;
-    case "EUR":
-      result = amount / rateEUR;
-      break;
-    case "GBP":
-      result = amount / rateGBP;
-      break;
-  }
-  resultElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(
-    2
-  )} ${currency}`;
-});
+    resultElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(
+      2
+    )} ${currency}`;
+  });
+
+  welcome();
+}
